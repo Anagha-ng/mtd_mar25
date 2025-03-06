af14dcfaa849d4350bcb05b4754a1c209363a900
Anagha-ng <ngangha3@gmail.com>
2025-03-06 14:33:15 +0530

    p2.c

---
+++ day1/p1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int intnum = 0;
+    puts("enter a number to check if its ");
+    scanf("%d", &intnumm);
+
+    if(intnum % 2 == o)
+    {
+        printf("%d is even", intnum);
+    }
+
+    else
+    {
+        printf("%d is not even", intnum);
+    }
+}
